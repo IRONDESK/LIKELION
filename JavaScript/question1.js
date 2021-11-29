9b57e43fa881984e10d9fbf8fc4b9c7942430e17
SOOCHUL SON <87234410+IRONDESK@users.noreply.github.com>
2021-11-29 23:43:55 +0900

    Add files via upload

---
+++ JavaScript/question1.js
@@ -0,0 +1,20 @@
+// let result = 0
+// for (let x = 0; x < 101; ++x) {
+//     //console.log(x);
+//     if (x % 3 == 0 || x % 5 == 0) {
+//         //console.log(x);
+//         result += x //result = result + x
+//     }
+// }
+
+// console.log(result)
+
+// || or
+// && and
+// !() ()의 부정
+
+let text = 'javascript';
+
+for (let i = 0; i < text.length; i ++) {
+    console.log(text[i], text[i+1]);
+}
